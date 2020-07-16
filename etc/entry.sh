109f0c78f3cfe6e83c26b9978bc640c97a21c19f
Timo <timorcb@gmail.com>
2020-07-16 15:27:18 -0400

    Update entry.sh

--- etc/entry.sh
+++ etc/entry.sh
@@ -7,6 +7,9 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 # Change hostname on first launch (you can comment this out if it has done it's purpose)
 sed -i -e 's/{{SERVER_HOSTNAME}}/'"${SRCDS_HOSTNAME}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
 
+# Copy copy configuration files from volume
+cp -r /cfg "${STEAMAPPDIR}/${STEAMAPP}"
+
 bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
                         -steam_dir "${STEAMCMDDIR}" \
                         -steamcmd_script "${STEAMAPPDIR}/${STEAMAPP}_update.txt" \
